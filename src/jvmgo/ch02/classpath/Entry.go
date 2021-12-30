298d3f3b7ca8eadd7796b1e24f0530d28e292bcc
Junglelk <1198581227@qq.com>
2021-12-30 14:26:52 +0800

    搜索class文件-补充注释

--- src/jvmgo/ch02/classpath/Entry.go
+++ src/jvmgo/ch02/classpath/Entry.go
@@ -8,6 +8,7 @@ const pathListSeparator = string(os.PathSeparator)
 // Entry 接口定义语法
 type Entry interface {
 	// readClass 函数定义方式: functionName(paramName paramType) (returnValue01, returnValue02, ...)
+	// go语言的参数可以拥有多个返参
 	readClass(className string) ([]byte, Entry, error)
 	// String 由上可知，此方法返回一个string，无参数
 	String() string
