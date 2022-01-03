b7cecc989e7c083309e5ebf76b63273ef2c3a7be
Jungle <1198581227@qq.com>
2022-01-03 19:49:22 +0800

    修改主函数

--- src/jvmgo/ch02/main.go
+++ src/jvmgo/ch02/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"jvmgo/jvmgo/ch02/classpath"
+	"strings"
+)
 
 func main() {
 	cmd := parseCmd()
@@ -14,5 +18,14 @@ func main() {
 }
 
 func startJVM(cmd *Cmd) {
-	fmt.Printf("classpath:%s class:%s args:%v\n", cmd.cpOption, cmd.class, cmd.args)
+	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+	fmt.Printf("classpath:%v class:%v args:%v \n", cp, cmd.class, cmd.args)
+	// Replace函数将字符串中的old(此处为".")替换为new(此处为"/")，替换进行n次，当n小于0时，没有次数限制
+	className := strings.Replace(cmd.class, ".", "/", -1)
+	classData, _, err := cp.ReadClass(className)
+	if err != nil {
+		fmt.Printf("Could not find or load main class %s \n", cmd.class)
+		return
+	}
+	fmt.Printf("class data:%v\n", classData)
 }
