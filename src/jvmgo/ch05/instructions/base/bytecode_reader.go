3df07ab61886bb80723b9e14191a67e2cbad4f5b
Jungle <1198581227@qq.com>
2022-01-28 11:37:39 +0800

    第五章-读取指令

---
+++ src/jvmgo/ch05/instructions/base/bytecode_reader.go
@@ -0,0 +1,44 @@
+package base
+
+// BytecodeReader code字段存放字节码，pc字段记录读取到了哪个字节。
+type BytecodeReader struct {
+	code []byte
+	pc   int
+}
+
+func (e *BytecodeReader) Reset(code []byte, pc int) {
+	e.code = code
+	e.pc = pc
+}
+
+func (e *BytecodeReader) ReadUint8() uint8 {
+	i := e.code[e.pc]
+	e.pc++
+	return i
+}
+
+func (e *BytecodeReader) ReadInt8() int8 {
+	return int8(e.ReadUint8())
+}
+
+func (e *BytecodeReader) ReadUint16() uint16 {
+	byte1 := uint16(e.code[e.ReadUint8()])
+	byte2 := uint16(e.code[e.ReadUint8()])
+	return (byte1 << 8) | byte2
+}
+
+func (e *BytecodeReader) ReadInt16() int16 {
+	return int16(e.ReadUint16())
+}
+
+func (e *BytecodeReader) ReadUint32() uint32 {
+	byte1 := uint32(e.code[e.ReadUint8()])
+	byte2 := uint32(e.code[e.ReadUint8()])
+	byte3 := uint32(e.code[e.ReadUint8()])
+	byte4 := uint32(e.code[e.ReadUint8()])
+	return (byte1 << 24) | (byte2 << 16) | (byte3 << 8) | byte4
+}
+
+func (e *BytecodeReader) ReadInt32() int32 {
+	return int32(e.ReadUint32())
+}
