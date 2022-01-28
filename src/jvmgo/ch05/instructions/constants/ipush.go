a1cd644b3ca45bcf9f2958ecb115f1615a0e6c72
Jungle <1198581227@qq.com>
2022-01-28 14:58:17 +0800

    第五章-整型转换

---
+++ src/jvmgo/ch05/instructions/constants/ipush.go
@@ -0,0 +1,37 @@
+package constants
+
+import (
+	"jvmgo/jvmgo/ch05/instructions/base"
+	"jvmgo/jvmgo/ch05/rtda"
+)
+
+/*
+	bipush 指令从操作数中获取一个byte型整数，扩展成int型，然后推入栈顶；
+	sipush 指令从操作数中获取一个short型整数，扩展成int型，然后推入栈顶。
+*/
+
+type BIPUSH struct {
+	val int8
+}
+
+type SIPUSH struct {
+	val int16
+}
+
+func (e *BIPUSH) FetchOperands(reader *base.BytecodeReader) {
+	e.val = reader.ReadInt8()
+}
+
+func (e *BIPUSH) Execute(frame *rtda.Frame) {
+	i := int32(e.val)
+	frame.OperandStack().PushInt(i)
+}
+
+func (e *SIPUSH) FetchOperands(reader *base.BytecodeReader) {
+	e.val = reader.ReadInt16()
+}
+
+func (e *SIPUSH) Execute(frame *rtda.Frame) {
+	i := int32(e.val)
+	frame.OperandStack().PushInt(i)
+}
