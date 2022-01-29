5ae1a828f1696037c66ed28a49de7793fbedc19a
Jungle <1198581227@qq.com>
2022-01-29 14:43:35 +0800

    第五章-swap指令

---
+++ src/jvmgo/ch05/instructions/stack/swap.go
@@ -0,0 +1,19 @@
+package stack
+
+import (
+	"jvmgo/jvmgo/ch05/instructions/base"
+	"jvmgo/jvmgo/ch05/rtda"
+)
+
+// SWAP 该指令交换栈顶两个变量
+type SWAP struct {
+	base.NoOperandsInstruction
+}
+
+func (e *SWAP) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	slot1 := stack.PopSlot()
+	slot2 := stack.PopSlot()
+	stack.PushSlot(slot1)
+	stack.PushSlot(slot2)
+}
