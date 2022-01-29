5b025bbd83d3aa2e6325c1cc03ce0a784b17ad84
Jungle <1198581227@qq.com>
2022-01-30 00:08:09 +0800

    temp

---
+++ src/jvmgo/ch05/instructions/math/sh.go
@@ -0,0 +1,27 @@
+package math
+
+import "jvmgo/jvmgo/ch05/instructions/base"
+
+type ISHL struct {
+	base.NoOperandsInstruction
+}
+
+type ISHR struct {
+	base.NoOperandsInstruction
+}
+
+type IUSHR struct {
+	base.NoOperandsInstruction
+}
+
+type LSHL struct {
+	base.NoOperandsInstruction
+}
+
+type LSHR struct {
+	base.NoOperandsInstruction
+}
+
+type LUSHR struct {
+	base.NoOperandsInstruction
+}
