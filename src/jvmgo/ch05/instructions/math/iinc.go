eaa6cdcc9d1e35fd507da596f2b3a4e8d5f2b1c1
Jungle <1198581227@qq.com>
2022-02-08 22:17:07 +0800

    第五章-iinc

---
+++ src/jvmgo/ch05/instructions/math/iinc.go
@@ -0,0 +1,29 @@
+package math
+
+import (
+	"jvmgo/jvmgo/ch05/instructions/base"
+	"jvmgo/jvmgo/ch05/rtda"
+)
+
+/*
+	iinc指令给局部变量表中的int变量添加常量值，局部量表索引和常量值都由指令的操作数提供。
+*/
+
+type IINC struct {
+	index uint
+	Const int32
+}
+
+// FetchOperands 方法从字节码中获取操作数
+func (e *IINC) FetchOperands(reader *base.BytecodeReader) {
+	e.index = uint(reader.ReadUint8())
+	e.Const = int32(reader.ReadInt8())
+}
+
+// Execute 从局部变量表中获取变量，给他加上常量值，再将结果写回局部变量表
+func (e *IINC) Execute(frame *rtda.Frame) {
+	localVars := frame.LocalVars()
+	val := localVars.GetInt(e.index)
+	val += e.Const
+	localVars.SetInt(e.index, val)
+}
