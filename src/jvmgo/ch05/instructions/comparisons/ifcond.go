06720f81d487a85d03b0d62e54f359be90f259e0
Jungle <1198581227@qq.com>
2022-02-09 22:29:20 +0800

    第五章-ifcond

---
+++ src/jvmgo/ch05/instructions/comparisons/ifcond.go
@@ -0,0 +1,64 @@
+package comparisons
+
+import (
+	"jvmgo/jvmgo/ch05/instructions/base"
+	"jvmgo/jvmgo/ch05/rtda"
+)
+
+type IFEQ struct {
+	base.BranchInstruction
+}
+type IFNE struct {
+	base.BranchInstruction
+}
+type IFLT struct {
+	base.BranchInstruction
+}
+type IFLE struct {
+	base.BranchInstruction
+}
+type IFGT struct {
+	base.BranchInstruction
+}
+type IFGE struct {
+	base.BranchInstruction
+}
+
+func (e *IFEQ) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val == 0 {
+		base.Branch(frame, e.Offset)
+	}
+}
+
+func (e *IFNE) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val != 0 {
+		base.Branch(frame, e.Offset)
+	}
+}
+func (e *IFLT) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val < 0 {
+		base.Branch(frame, e.Offset)
+	}
+}
+func (e *IFLE) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val <= 0 {
+		base.Branch(frame, e.Offset)
+	}
+}
+
+func (e *IFGT) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val > 0 {
+		base.Branch(frame, e.Offset)
+	}
+}
+func (e *IFGE) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val >= 0 {
+		base.Branch(frame, e.Offset)
+	}
+}
