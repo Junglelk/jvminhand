e1834f44483280c6494cb94b29a00f7bd7a1c4c1
Jungle <1198581227@qq.com>
2022-02-09 23:16:30 +0800

    第五章-goto

---
+++ src/jvmgo/ch05/instructions/control/goto.go
@@ -0,0 +1,20 @@
+package control
+
+import (
+	"jvmgo/jvmgo/ch05/instructions/base"
+	"jvmgo/jvmgo/ch05/rtda"
+)
+
+/*
+	控制指令有 11 条，jsr和ret指令在Java6之后就不再用来实现finally子句，
+	return系列指令有6条，用于从方法调用中返回。本章不实现。
+	剩下三条为：goto、tableswitch、lookupswitch
+*/
+
+type GOTO struct {
+	base.BranchInstruction
+}
+
+func (e *GOTO) Execute(frame *rtda.Frame) {
+	base.Branch(frame, e.Offset)
+}
