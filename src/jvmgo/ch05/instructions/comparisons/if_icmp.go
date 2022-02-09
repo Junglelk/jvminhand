3442b66c859436f68ff6b237f546fc8d9a745bbc
Jungle <1198581227@qq.com>
2022-02-09 23:00:09 +0800

    第五章-if_icmp

---
+++ src/jvmgo/ch05/instructions/comparisons/if_icmp.go
@@ -0,0 +1,79 @@
+package comparisons
+
+import (
+	"jvmgo/jvmgo/ch05/instructions/base"
+	"jvmgo/jvmgo/ch05/rtda"
+)
+
+type IF_ICMPEQ struct {
+	base.BranchInstruction
+}
+
+type IF_ICMPNE struct {
+	base.BranchInstruction
+}
+
+type IF_ICMPLT struct {
+	base.BranchInstruction
+}
+
+type IF_ICMPLE struct {
+	base.BranchInstruction
+}
+
+type IF_ICMPGT struct {
+	base.BranchInstruction
+}
+
+type IF_ICMPGE struct {
+	base.BranchInstruction
+}
+
+func (e *IF_ICMPEQ) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	if v1 == v2 {
+		base.Branch(frame, e.Offset)
+	}
+}
+func (e *IF_ICMPNE) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	if v1 != v2 {
+		base.Branch(frame, e.Offset)
+	}
+}
+func (e *IF_ICMPLT) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	if v1 < v2 {
+		base.Branch(frame, e.Offset)
+	}
+}
+func (e *IF_ICMPLE) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	if v1 <= v2 {
+		base.Branch(frame, e.Offset)
+	}
+}
+func (e *IF_ICMPGT) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	if v1 > v2 {
+		base.Branch(frame, e.Offset)
+	}
+}
+func (e *IF_ICMPGE) Execute(frame *rtda.Frame) {
+	stack := frame.OperandStack()
+	v2 := stack.PopInt()
+	v1 := stack.PopInt()
+	if v1 >= v2 {
+		base.Branch(frame, e.Offset)
+	}
+}
