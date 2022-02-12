7a41059c8e052b3a48c0a93df11274f2d3b59bfb
Jungle <1198581227@qq.com>
2022-02-12 22:51:43 +0800

    一点优化

--- src/jvmgo/ch05/instructions/comparisons/if_icmp.go
+++ src/jvmgo/ch05/instructions/comparisons/if_icmp.go
@@ -30,50 +30,45 @@ type IF_ICMPGE struct {
 }
 
 func (e *IF_ICMPEQ) Execute(frame *rtda.Frame) {
-	stack := frame.OperandStack()
-	v2 := stack.PopInt()
-	v1 := stack.PopInt()
+	v1, v2 := _cmpPop(frame)
 	if v1 == v2 {
 		base.Branch(frame, e.Offset)
 	}
 }
 func (e *IF_ICMPNE) Execute(frame *rtda.Frame) {
-	stack := frame.OperandStack()
-	v2 := stack.PopInt()
-	v1 := stack.PopInt()
+	v1, v2 := _cmpPop(frame)
 	if v1 != v2 {
 		base.Branch(frame, e.Offset)
 	}
 }
 func (e *IF_ICMPLT) Execute(frame *rtda.Frame) {
-	stack := frame.OperandStack()
-	v2 := stack.PopInt()
-	v1 := stack.PopInt()
+	v1, v2 := _cmpPop(frame)
 	if v1 < v2 {
 		base.Branch(frame, e.Offset)
 	}
 }
 func (e *IF_ICMPLE) Execute(frame *rtda.Frame) {
-	stack := frame.OperandStack()
-	v2 := stack.PopInt()
-	v1 := stack.PopInt()
+	v1, v2 := _cmpPop(frame)
 	if v1 <= v2 {
 		base.Branch(frame, e.Offset)
 	}
 }
 func (e *IF_ICMPGT) Execute(frame *rtda.Frame) {
-	stack := frame.OperandStack()
-	v2 := stack.PopInt()
-	v1 := stack.PopInt()
+	v1, v2 := _cmpPop(frame)
 	if v1 > v2 {
 		base.Branch(frame, e.Offset)
 	}
 }
 func (e *IF_ICMPGE) Execute(frame *rtda.Frame) {
-	stack := frame.OperandStack()
-	v2 := stack.PopInt()
-	v1 := stack.PopInt()
+	v1, v2 := _cmpPop(frame)
 	if v1 >= v2 {
 		base.Branch(frame, e.Offset)
 	}
+}
+
+func _cmpPop(frame *rtda.Frame) (v1, v2 int32) {
+	stack := frame.OperandStack()
+	v2 = stack.PopInt()
+	v1 = stack.PopInt()
+	return
 }
