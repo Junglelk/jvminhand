e8603b47a344285a0760382ce1bf202a701e1d4e
Jungle <1198581227@qq.com>
2022-02-19 21:59:52 +0800

    第六章-类和对象-class文件-类成员-字段信息

---
+++ src/jvmgo/ch06/rtda/heap/field.go
@@ -0,0 +1,23 @@
+package heap
+
+import "jvmgo/jvmgo/ch06/classfile"
+
+/*
+	字段信息
+*/
+
+// Field 目前信息完全由类成员信息构成
+type Field struct {
+	ClassMember
+}
+
+// newFields 创建字段表 MemberInfo 字段和方法表。字段和方法的基本结构大致相同，不同的仅有属性表
+func newFields(class *Class, cfFields []*classfile.MemberInfo) []*Field {
+	fields := make([]*Field, len(cfFields))
+	for i, cfField := range cfFields {
+		fields[i] = &Field{}
+		fields[i].class = class
+		fields[i].copyMemberInfo(cfField)
+	}
+	return fields
+}
